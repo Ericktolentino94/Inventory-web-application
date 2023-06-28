42625b50c4defef5d529a680957471d23b7f7b45
Erick Tolentino <ericktolentino@pursuit.org>
2023-06-28 13:57:35 -0400

    fixed main js

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const form = document.querySelector("form");
 
-const li = document.querySelector("li")
 
 
-const submit = document.querySelector("#submitButton")
 
+// function generateSneaker(item){
 form.addEventListener("submit",(event) => {
+    let li = document.createElement("li")
     event.preventDefault();
     let sneakerListing = event.target
-    li.append( sneakerListing.sneakerName.value);
-    li.append(sneakerListing.sneakerPrice.value);
-    li.append(sneakerListing.size.value);
+    let sneakerList = document.querySelector("ul")
+    let sneakerTitle = document.createElement("h3")
+    let sneakerPrice = document.createElement("p")
+    let sneakerSize = document.createElement("p")
     
+
+    sneakerTitle.textContent = sneakerListing.sneakerName.value
+    sneakerPrice.textContent = sneakerListing.sneakerPrice.value
+    sneakerSize.textContent = sneakerListing.sneakerSize.value
+    // console.log(sneakerList.sneakerSize.value)
+    li.append(sneakerTitle)
+    li.append(sneakerPrice);
+    li.append(sneakerSize);
+    
+    sneakerList.appendChild(li)
 })
+form.reset()
+// }
