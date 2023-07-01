7899745387546953478cadd68b2b2cc7b232cfc5
Erick Tolentino <ericktolentino@pursuit.org>
2023-07-01 15:29:09 -0400

    added some things to main.js and style.css and also edited the readme.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Inventory-web-application
+# Sneaker Inventory Tracking Application
+
+## What is it?
+I have created a sneaker tracking website that creates new profiles for a user.
+
+## How to use it
+Once the user inputs all the required information into the form, then just click submit. If you want to reset the form just hit the reset buttom. It will create a new inventory item that the user can now use to track their inventory for the business.
+
+## How will this help?
+This will be a great tool for anyone working in the sneaker industry as sellers we have to constantly be updating our inventory items to keep track of what's coming in and out.
+
+## How to delete an item
+If a user no longer has an Item in their inventory they can remove it by simply clicking the "Remove Item" button under the new sneaker.
--- main.js
+++ main.js
@@ -1,40 +1,65 @@
 const form = document.querySelector("form");
+const li = document.querySelector("li");
 
 
-
-
-// function generateSneaker(item){
-form.addEventListener("submit",(event) => {
-    let li = document.createElement("li")
-    event.preventDefault();
-    let sneakerListing = event.target
-    let sneakerList = document.querySelector("ul")
-    let sneakerTitle = document.createElement("h3")
-    let sneakerPrice = document.createElement("p")
-    let sneakerSize = document.createElement("p")
-    let sneakerGender = document.createElement("p")
-    let sneakerInstock = document.createElement("p")
-    let sneakerImage = document.createElement("img")
-
-    sneakerImage.append("src=")
-    console.log(sneakerImage)
-
-    sneakerTitle.textContent = sneakerListing.sneakerName.value
-    sneakerPrice.textContent = `Price: $ ${sneakerListing.sneakerPrice.value}`
-    sneakerInstock.textContent = `In Stock: ${sneakerListing.sneakerInstock.value}`
-    sneakerGender.textContent = `US ${sneakerListing.gender.value}`
-    sneakerSize.textContent = `Size: ${sneakerListing.sneakerSize.value}`
-    sneakerImage.src = sneakerListing.imgUrl.value
-
-    
-    li.append(sneakerTitle);
-    li.append(sneakerPrice);
-    li.append(sneakerInstock);
-    li.append(sneakerGender);
-    li.append(sneakerSize);
-    li.append(sneakerImage)
-    
-    sneakerList.appendChild(li)
+li.addEventListener("click", (event) => {
+    li.remove()
 })
-form.reset()
-// }
+
+
+form.addEventListener("submit", (event) => {
+    if (!form.value.innerText) {
+        alert("Error fields in form cannot be empty")
+    } else {
+  let li = document.createElement("li");
+  event.preventDefault();
+  const sneakerListing = event.target;
+  const sneakerList = document.querySelector("ul");
+  const sneakerTitle = document.createElement("h3");
+  const sneakerPrice = document.createElement("p");
+  const sneakerSize = document.createElement("p");
+  const sneakerGender = document.createElement("p");
+  const sneakerInstock = document.createElement("p");
+  const sneakerImage = document.createElement("img");
+  const sneakerRemove = document.createElement("button");
+
+  sneakerImage.append("src=");
+  sneakerRemove.append("Remove Item");
+
+
+  sneakerTitle.textContent = sneakerListing.sneakerName.value;
+  sneakerPrice.textContent = `Price: $ ${sneakerListing.sneakerPrice.value}`;
+  sneakerInstock.textContent = `In Stock: ${sneakerListing.sneakerInstock.value}`;
+  sneakerGender.textContent = `US ${sneakerListing.gender.value}`;
+  sneakerSize.textContent = `Size: ${sneakerListing.sneakerSize.value}`;
+  sneakerImage.src = sneakerListing.imgUrl.value;
+
+
+  sneakerRemove.addEventListener("click", (event) => {
+    document.querySelectorAll("li");
+    li.remove();
+  });
+
+  sneakerInstock.addEventListener("click", (event) => {
+    if(sneakerInstock.textContent === "True") {
+        sneakerInstock.textContent = "False"
+    } else {
+        if(sneakerInstock.textContent === "False") {
+            sneakerInstock.textContent = "True"
+        }
+    }
+    });
+
+  li.append(sneakerImage);
+  li.append(sneakerTitle);
+  li.append(sneakerPrice);
+  li.append(sneakerInstock);
+  li.append(sneakerGender);
+  li.append(sneakerSize);
+  li.append(sneakerRemove);
+
+  sneakerList.appendChild(li);
+}});
+    
+form.reset();
+
